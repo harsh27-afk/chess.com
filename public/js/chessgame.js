eba23a8b86be35b4b3b6d8809bfe9ecd97c1d243
harsh27-afk <itisharsh27@gmail.com>
2025-01-10 12:03:39 +0530

    socket events attached

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,5 @@
+// const { render } = require("ejs");
+
 const socket=io();
 const chess= new Chess();
 
@@ -27,10 +29,93 @@ const renderBoard=()=>{
                     "piece",
                     square.color==="w"?"white":"black",
                 );
-                pieceElement.innerText="";
+                pieceElement.innerText=getPieceUnicode(square);
                 pieceElement.draggable=playerRole===square.color;
+
+                pieceElement.addEventListener("dragstart",(e)=>{
+                    if(pieceElement.draggable){
+                        draggedPiece=pieceElement;
+                        sourceSquare={
+                            row:rowIndex,
+                            col:squareIndex
+                        }
+                        e.dataTransfer.setData("text/plain","");
+                    }
+                })
+                pieceElement.addEventListener("dragend",(e)=>{
+                    draggedPiece=null;
+                    sourceSquare=null;
+                });
+                squareElement.appendChild(pieceElement);
              }
 
+                squareElement.addEventListener("dragover",(e)=>{
+                    e.preventDefault();
+                });
+
+                squareElement.addEventListener("drop",(e)=>{
+                    e.preventDefault();
+                    if(draggedPiece){
+                        const targetSource={
+                            row:parseInt(squareElement.dataset.row),
+                            col:parseInt(squareElement.dataset.col)
+                        };
+
+                        handleMove(sourceSquare,targetSource);  
+                    }
+                })
+                boardElement.appendChild(squareElement)
         })
+       
     })
 }
+
+const handleMove=(source,target)=>{
+    const move={
+        from:`${String.fromCharCode(97+source.col)}${8-source.row}`,
+        to:`${String.fromCharCode(97+target.col)}${8-target.row}`,
+        promotion:"q"
+    }
+}
+
+const getPieceUnicode=(piece)=>{
+    const unicodePieces={
+        p:"♙",
+        r:"♖",
+        n:"♘",
+        b:"♗",
+        q:"♕",
+        k:"♔",
+        P:"♟",
+        R:"♜",
+        N:"♞",
+        B:"♝",
+        Q:"♛",
+        K:"♚",
+    };
+    
+    return unicodePieces[piece.type] || "";
+}
+
+socket.on("playerRole",(role)=>{
+    playerRole=role;
+    renderBoard();
+})
+
+socket.on("spectatorRole",()=>{
+    playerRole=null;
+    renderBoard();
+})
+
+socket.on("boardState",(fen)=>{
+    chess.load(fen);
+    renderBoard();
+})
+
+socket.on("move",(move)=>{
+    chess.move(move);
+    renderBoard();
+})
+
+
+renderBoard();
